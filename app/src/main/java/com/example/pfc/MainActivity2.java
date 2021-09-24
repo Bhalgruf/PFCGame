7ec855d68c1d467b680479731e6ebc97652bd02f
Bhalgruf <Bhalgruflecon@gmail.com>
2021-09-24 17:40:07 +0200

    activitydebase

--- app/src/main/java/com/example/pfc/MainActivity2.java
+++ app/src/main/java/com/example/pfc/MainActivity2.java
@@ -2,13 +2,31 @@ package com.example.pfc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button btn_co;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        btn_co =(Button)findViewById(R.id.btn_co);
+
+        btn_co.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, Menu.class);
+                startActivity(intent);
+            }
+
+        });
+
+
     }
 }
