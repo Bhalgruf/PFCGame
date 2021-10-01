1bbaa4fedd28fc01f3d5cbcec89ae71e68bd3a6e
Bhalgruf <Bhalgruflecon@gmail.com>
2021-10-01 08:20:12 +0200

    activité_ok

--- app/src/main/java/com/example/pfc/Jeu1.java
+++ app/src/main/java/com/example/pfc/Jeu1.java
@@ -3,12 +3,26 @@ package com.example.pfc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Jeu1 extends AppCompatActivity {
+
+    Button quitter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jeu1);
+
+        quitter =(Button)findViewById(R.id.btn_quitter);
+
+
+    }
+
+    public void onClick5(View view) {
+
+        finish();
+
     }
 }
--- app/src/main/java/com/example/pfc/Menu.java
+++ app/src/main/java/com/example/pfc/Menu.java
@@ -2,13 +2,33 @@ package com.example.pfc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
-
+    Button btn_retour1;
+    Button Jeu1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_menu);
+        btn_retour1 =(Button)findViewById(R.id.btn_retour1);
+        Jeu1 =(Button)findViewById(R.id.btn_jeu1);
+
+    }
+
+    public void onClick1(View view) {
+        Intent intent = new Intent(Menu.this, Jeu1.class);
+        startActivity(intent);
+
+    }
+
+    public void onClick4(View view) {
+
+        finish();
+
     }
 }
