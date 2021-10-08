422b36ce119b02a485dfc603e17a14c53371d6a2
JustAlfa <maximaudren@orange.fr>
2021-10-08 11:46:10 +0200

    design 3

--- app/src/main/java/com/example/pfc/Classement.java
+++ app/src/main/java/com/example/pfc/Classement.java
@@ -3,19 +3,21 @@ package com.example.pfc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Classement extends AppCompatActivity {
 
-    private Button Retour;
+   public ImageButton Retour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classement);
-        Retour=(Button) findViewById(R.id.btn_retourMenu);
+        Retour=findViewById(R.id.btn_retourClassement);
 
 
     }
@@ -23,7 +25,7 @@ public class Classement extends AppCompatActivity {
     public void onClickClassement(View view) {
 
         switch (view.getId()) {
-            case R.id.btn_retourMenu:
+            case R.id.btn_retourClassement:
                 Intent Menu = new Intent(Classement.this, Menu.class);
                 startActivity(Menu);
                 break;
